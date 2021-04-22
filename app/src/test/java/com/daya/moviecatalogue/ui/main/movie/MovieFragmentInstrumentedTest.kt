616eddd2fccf033d89121acded385cedd9ebfce4
ninggahaken <daya.aeist@gmail.com>
2021-04-22 13:52:44 +0700

    update test should intent into detail

--- app/src/test/java/com/daya/moviecatalogue/ui/main/movie/MovieFragmentInstrumentedTest.kt
+++ app/src/test/java/com/daya/moviecatalogue/ui/main/movie/MovieFragmentInstrumentedTest.kt
@@ -1,16 +1,24 @@
 package com.daya.moviecatalogue.ui.main.movie
 
+import android.content.Intent
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.daya.moviecatalogue.R
 import com.daya.moviecatalogue.data.DataDummy
+import com.daya.moviecatalogue.ui.MovieCatApplication
+import com.daya.moviecatalogue.ui.detail.DetailActivity
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Shadows
 
 @RunWith(AndroidJUnit4::class)
 class MovieFragmentInstrumentedTest{
@@ -23,17 +31,23 @@ class MovieFragmentInstrumentedTest{
         onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
                 dummyMovie.size
-            )
+            ),click()
         )
     }
 
     @Test
     fun `item recyclerview movie click should intent into detail`() {
-       launchFragmentInContainer<MovieFragment>()
+       val scenario = launchFragmentInContainer<MovieFragment>()
         onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyMovie.size
-            )
+                0
+            ),click()
         )
+        scenario.onFragment{
+            val expectedIntent = Intent(it.context, DetailActivity::class.java)
+            val app = ApplicationProvider.getApplicationContext<MovieCatApplication>()
+            val actual = Shadows.shadowOf(app).nextStartedActivity
+            assertThat(expectedIntent.component).isEqualTo(actual.component)
+        }
     }
 }
--- app/src/test/java/com/daya/moviecatalogue/ui/main/tvshow/TvShowFragmentInstrumentedTest.kt
+++ app/src/test/java/com/daya/moviecatalogue/ui/main/tvshow/TvShowFragmentInstrumentedTest.kt
@@ -1,17 +1,23 @@
 package com.daya.moviecatalogue.ui.main.tvshow
 
+import android.content.Intent
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.daya.moviecatalogue.R
 import com.daya.moviecatalogue.data.DataDummy
+import com.daya.moviecatalogue.ui.MovieCatApplication
+import com.daya.moviecatalogue.ui.detail.DetailActivity
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Shadows
 
 @RunWith(AndroidJUnit4::class)
 class TvShowFragmentInstrumentedTest{
@@ -31,11 +37,17 @@ class TvShowFragmentInstrumentedTest{
 
     @Test
     fun `item recyclerview movie click should intent into detail`() {
-        launchFragmentInContainer<TvShowFragment>()
+        val scenario = launchFragmentInContainer<TvShowFragment>()
         onView(ViewMatchers.withId(R.id.rv_movie)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
                 dummyTvShow.size
-            )
+            ),click()
         )
+        scenario.onFragment{
+            val expectedIntent = Intent(it.context, DetailActivity::class.java)
+            val app = ApplicationProvider.getApplicationContext<MovieCatApplication>()
+            val actual = Shadows.shadowOf(app).nextStartedActivity
+            assertThat(expectedIntent.component).isEqualTo(actual.component)
+        }
     }
 }
