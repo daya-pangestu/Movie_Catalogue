22616c05ac266ad8bed4eea48ed4f39da5c49018
ninggahaken <daya.aeist@gmail.com>
2021-04-22 11:59:32 +0700

    add DetailActivity.kt

---
+++ app/src/main/java/com/daya/moviecatalogue/ui/main/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.daya.moviecatalogue.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daya.moviecatalogue.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
