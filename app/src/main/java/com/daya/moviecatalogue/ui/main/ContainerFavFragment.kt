6facc7b35c26525b611293fd46f98cd2d7e63ced
ninggahaken <daya.aeist@gmail.com>
2021-05-19 16:14:56 +0700

    update = container favorite

--- app/src/main/java/com/daya/moviecatalogue/ui/main/ContainerFavFragment.kt
+++ app/src/main/java/com/daya/moviecatalogue/ui/main/ContainerFavFragment.kt
@@ -10,7 +10,7 @@ import com.daya.moviecatalogue.ui.main.favorite.movie.MovieFavFragment
 import com.daya.moviecatalogue.ui.main.favorite.tvshow.TvShowFavFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
-class ContainerFavFragment : Fragment(R.layout.fragment_item_list) {
+class ContainerFavFragment : Fragment(R.layout.fragment_container) {
 
     private val binding by viewBinding<FragmentContainerBinding>()
 
