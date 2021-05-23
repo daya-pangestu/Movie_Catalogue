ea13c0d9b624b1d94af568ef5feca50893199813
ad <da>
2021-05-23 15:10:29 +0700

    unit test against MovieViewModelTest.kt

--- app/src/test/java/com/daya/moviecatalogue/ui/main/favorite/movie/MovieFavViewModelTest.kt
+++ app/src/test/java/com/daya/moviecatalogue/ui/main/favorite/movie/MovieFavViewModelTest.kt
@@ -3,21 +3,13 @@ package com.daya.moviecatalogue.ui.main.favorite.movie
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.daya.moviecatalogue.data.LocalPersistRepository
-import com.daya.moviecatalogue.data.main.movie.Movie
-import com.daya.moviecatalogue.data.main.movie.local.MovieDao
-import com.daya.moviecatalogue.mapToMovieEntity
 import com.daya.moviecatalogue.shared.DataDummy
 import com.daya.moviecatalogue.shared.MainCoroutineRule
 import com.daya.moviecatalogue.shared.getOrAwaitValue
-import com.daya.moviecatalogue.shared.observeForTesting
 import com.google.common.truth.Truth.assertThat
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
-import okhttp3.internal.toImmutableList
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -36,7 +28,7 @@ class MovieFavViewModelTest {
     @get:Rule(order = 2)
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var movieFavViewModel: MovieFavViewModel
+    private lateinit var movieFavFavViewModel: MovieFavViewModel
 
     private lateinit var localPersistRepository: LocalPersistRepository
 
@@ -46,13 +38,13 @@ class MovieFavViewModelTest {
     @Before
     fun setUp() {
         localPersistRepository = mock()
-        movieFavViewModel = MovieFavViewModel(localPersistRepository)
+        movieFavFavViewModel = MovieFavViewModel(localPersistRepository)
     }
 
     @Test
     fun `verify localPersistRepository#getAllFavoriteMovies get called when favoriteMovies invoked`() = mainCoroutineRule.testDispatcher.runBlockingTest{
         whenever(localPersistRepository.getAllFavoriteMovies()).thenReturn(dummyFlowMovies)
-        val actual = movieFavViewModel.favoriteMovies.getOrAwaitValue()
+        val actual = movieFavFavViewModel.favoriteMovies.getOrAwaitValue()
 
         verify(localPersistRepository).getAllFavoriteMovies()
         assertThat(actual).isEqualTo(dummyListMovies)
--- app/src/test/java/com/daya/moviecatalogue/ui/main/foryou/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/daya/moviecatalogue/ui/main/foryou/movie/MovieViewModelTest.kt
@@ -1,5 +1,61 @@
 package com.daya.moviecatalogue.ui.main.foryou.movie
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.daya.moviecatalogue.data.LocalPersistRepository
+import com.daya.moviecatalogue.data.Resource
+import com.daya.moviecatalogue.data.main.MainRepository
+import com.daya.moviecatalogue.shared.DataDummy
+import com.daya.moviecatalogue.shared.MainCoroutineRule
+import com.daya.moviecatalogue.shared.getOrAwaitValue
+import com.daya.moviecatalogue.shared.observeForTesting
+import com.daya.moviecatalogue.ui.main.favorite.movie.MovieFavViewModel
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
-class MovieViewModelTest
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class MovieViewModelTest{
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var mainRepository: MainRepository
+    private lateinit var movieViewModel: MovieViewModel
+
+    private val dummyListMovies = DataDummy.getListMovie()
+
+    @Before
+    fun setUp() {
+        mainRepository = mock()
+        movieViewModel = MovieViewModel(mainRepository)
+    }
+
+    @Test
+    fun `movieViewModel#discoverMovie should return resSucces listMovies`() = mainCoroutineRule.testDispatcher.runBlockingTest{
+        whenever(mainRepository.discoverMovies()).thenReturn(dummyListMovies)
+
+        //initial value
+        val actualResLoading = movieViewModel.discoverMovie.getOrAwaitValue()
+        assertThat(actualResLoading).isEqualTo(Resource.Loading)
+
+        //latest value
+        movieViewModel.discoverMovie.observeForTesting {
+            assertThat(movieViewModel.discoverMovie.value).isEqualTo(Resource.Success(dummyListMovies))
+        }
+        verify(mainRepository).discoverMovies()
+    }
+}
